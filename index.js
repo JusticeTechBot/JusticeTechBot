c0156dae724e10e6803bb2e1cc970cfc7b939917
JusticeTechBot <155928243+JusticeTechBot@users.noreply.github.com>
2024-04-14 14:29:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ setTimeout(() => {
         const sockOptions = {
             version,
             logger: pino({ level: "silent" }),
-            browser: ['Zokou-Md', "safari", "1.0.0"],
+            browser: ['JusticeTechBot', "safari", "1.0.0"],
             printQRInTerminal: true,
             fireInitQueries: false,
             shouldSyncHistoryMessage: true,
